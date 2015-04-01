f437e3735a1b0a0e13ecf7a053d9c4dad7fd1bb8
kaloyanbobev <kaloyanbobev@gmail.com>
2015-04-01 13:33:47 +0300

    adding requireJS

---
+++ requireJS/scripts/app.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    require.config({
+        paths: {
+            "jquery": "libs/jquery",
+            "handlebars": "libs/handlebars",
+            "controls": "controls",
+        }
+    });
+    require(['controls', 'jquery', 'handlebars'], function (Controls,jq,Handlebars) {
+        
+        var people = [
+        { id: 1, name: "Doncho Minkov", age: 18, avatarUrl: "images/DonchoMinkov.png" },
+        { id: 2, name: "Gergi Gergiev", age: 19, avatarUrl: "images/Georgi-Georgiev-Photo.jpg" }];
+        var container = $('#comboBox'),
+        htmlTemplate = $('#person-template').html(),
+        comboBoxTemplate = Handlebars.compile(htmlTemplate);
+        var comboBox = new Controls.ComboBox({
+            container: container,
+            people: people,
+            template: comboBoxTemplate
+        });
+    });
+}());
