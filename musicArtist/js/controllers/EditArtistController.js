579e157a6a5d531a6de83d3f7fa2cbad23ad23d3
kaloyanbobev <kaloyanbobev@gmail.com>
2015-04-16 22:32:03 +0300

    new page, filter and contoller

---
+++ musicArtist/js/controllers/EditArtistController.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+musicApp.controller('EditArtistController',
+    function EditArtistController($scope) {
+        $scope.saveArtist = saveArtist;
+        $scope.cancel = cancel;
+
+        function saveArtist(artist) {
+            alert(artist.name + " " + artist.created);
+        }
+
+        function cancel() {
+            alert('cancel');
+        }
+
+    });
---
+++ musicArtist/js/filters/status.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+musicApp.filter('status', function () {
+    return function (input) {
+        switch (input) {
+            case 1: return "Sold out";
+                break;
+            case 2: return "Realased";
+                break;
+            case 3: return "Unrealased";
+                break;
+        }
+
+    }
+})
