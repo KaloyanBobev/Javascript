e084f4ad147aa3635d83e377cffe8a9c700036d4
kaloyanbobev <kaloyanbobev@gmail.com>
2015-05-03 21:44:33 +0300

    new page history.html and photo-gallery.html

--- communityCenter/js/app.js
+++ communityCenter/js/app.js
@@ -1,5 +1,14 @@
 'use strict';
 
 var newspaperApp = angular
-    .module('newspaperApp', [])
+    .module('newspaperApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/history', {
+                templateUrl: 'template/history.html'
+            })
+            .when('/photo-gallery', {
+                templateUrl: 'template/photo-gallery.html'
+            })
+    })
     .constant('author', 'Kaloyan Bobev');
