c6bd783f966502f1785ad157eb761810172a54d0
kaloyanbobev <kaloyanbobev@gmail.com>
2015-06-21 23:57:00 +0300

    add pages and images

---
+++ staraZagoraWeb/js/controllers/PageController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+websiteApp.controller('PageController',
+    function PageController($scope) {
+        $scope.author = 'Калоян Бобев';
+        $scope.year = '2015';
+        $scope.copyright = 'Всички права запазени.';
+    });
