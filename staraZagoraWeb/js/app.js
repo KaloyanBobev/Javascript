f3152f72e071dfcb211ef8a11e57ba22a11ed79f
kaloyanbobev <kaloyanbobev@gmail.com>
2015-06-21 21:36:34 +0300

    new images

--- staraZagoraWeb/js/app.js
+++ staraZagoraWeb/js/app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+
+var websiteApp = angular
+    .module('websiteApp', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'template/home.html'
+            })
+            .when('/landmarks', {
+                templateUrl: 'template/landmarks.html'
+            })
+            .when('/romanExcavations', {
+                templateUrl: 'template/romanExcavations.html'
+            })
+            .when('/museum', {
+                templateUrl: 'template/museum.html'
+            })
+            .when('/monuments', {
+                templateUrl: 'template/monuments.html'
+            })
+            .when('/zoopark', {
+                templateUrl: 'template/zoopark.html'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            })
+
+
+    });
