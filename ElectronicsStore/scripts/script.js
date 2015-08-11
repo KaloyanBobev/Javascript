40df7f550a428c1d1e092efe6e4e7b51d65ed3d5
kaloyanbobev <kaloyanbobev@gmail.com>
2015-08-11 11:43:47 +0300

    new website

---
+++ ElectronicsStore/scripts/script.js
@@ -0,0 +1,23 @@
+/*document.onload = function() {
+    alert('Hi');
+}*/
+
+(function() {
+    var leftNav,
+        ul,
+        li,
+        i,
+        len;
+
+    leftNav = document.getElementById('main_aside');
+    console.log(leftNav);
+    ul = document.createElement('ul');
+    for (i = 0, len = 5; i < len; i++) {
+        li = document.createElement('li');
+        li.innerHTML = i;
+        ul.appendChild(li);
+    }
+    leftNav.appendChild(ul);
+
+
+})();
