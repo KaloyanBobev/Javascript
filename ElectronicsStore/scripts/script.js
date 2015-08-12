0cd3cac124178f690cf0b6d6fce8266a1c4f11d5
kaloyanbobev <kaloyanbobev@gmail.com>
2015-08-12 23:45:45 +0300

    added new menu

--- ElectronicsStore/scripts/script.js
+++ ElectronicsStore/scripts/script.js
@@ -3,14 +3,14 @@
 }*/
 
 (function() {
-    var leftNav,
-        ul,
-        li,
-        i,
-        len;
+    var leftNav, ul, li,
+        i, len,
+        tv, tvMenu,
+        photo, photoMenu,
+        computer, computerMenu;
 
     leftNav = document.getElementById('main_aside');
-    console.log(leftNav);
+
     ul = document.createElement('ul');
     for (i = 0, len = 5; i < len; i++) {
         li = document.createElement('li');
@@ -18,5 +18,48 @@
         ul.appendChild(li);
     }
     leftNav.appendChild(ul);
+
+    tv = document.getElementById('category_0');
+    tv.addEventListener('mouseenter', function() {
+        tvMenu = document.getElementsByClassName('tv_menu');
+        for (var i = 0; i < tvMenu.length; i++) {
+            tvMenu[i].style.display = 'block';
+        }
+    });
+    tv.addEventListener('mouseleave', function() {
+        tvMenu = document.getElementsByClassName('tv_menu');
+        for (var i = 0; i < tvMenu.length; i++) {
+            tvMenu[i].style.display = 'none';
+        }
+    });
+
+    photo = document.getElementById('category_1');
+    photo.addEventListener('mouseenter', function() {
+        photoMenu = document.getElementsByClassName('photo_menu');
+        for (var i = 0; i < photoMenu.length; i++) {
+            photoMenu[i].style.display = 'block';
+        }
+    });
+    photo.addEventListener('mouseleave', function() {
+        photoMenu = document.getElementsByClassName('photo_menu');
+        for (var i = 0; i < photoMenu.length; i++) {
+            photoMenu[i].style.display = 'none';
+        }
+    });
+
+    computer = document.getElementById('category_2');
+    computer.addEventListener('mouseenter', function() {
+        computerMenu = document.getElementsByClassName('computer_menu');
+        for (var i = 0; i < computerMenu.length; i++) {
+            computerMenu[i].style.display = 'block';
+        }
+    });
+    computer.addEventListener('mouseleave', function() {
+        computerMenu = document.getElementsByClassName('computer_menu');
+        for (var i = 0; i < computerMenu.length; i++) {
+            computerMenu[i].style.display = 'none';
+        }
+    });
+
 
 })();
