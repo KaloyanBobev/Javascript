3787c8cfad4901bf25687c6b85a9938fe3fc9898
Kaloyan <kaloyanbobev@gmail.com>
2020-05-28 13:47:59 +0100

    Add files via upload
    changing the footer

--- communityCenter/js/controllers/PageController.js
+++ communityCenter/js/controllers/PageController.js
@@ -2,6 +2,7 @@
 
 newspaperApp.controller('PageController',
     function PageController($scope, author) {
+
         $scope.author = author;
-        $scope.year = '2015';
+        $scope.year = '2015-2020';
     });
