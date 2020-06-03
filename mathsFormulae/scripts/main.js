6b921b29ba19cec7b1dfb2f8870e604111bddb40
kaloyanbobev <kaloyanbobev@gmail.com>
2020-06-03 19:14:46 +0100

    improving the perimeter for square

--- mathsFormulae/scripts/main.js
+++ mathsFormulae/scripts/main.js
@@ -17,28 +17,40 @@ let nav = new Vue({
 
 });
 
-let info = new Vue({
-    el: `#info`,
-    data: {
-        header: `The perimeter of Square`,
-        formula: `A Square is a regular rectangle but have four equal sides A with 90 degrees angles
-        P = 4A
-        A is the length of one of the sides.`
-    }
-});
-
-$(document).ready(function () {
+$(document).ready(function() {
     $('#menubar').menu();
 
     $('#menubar').menu({
         position: { my: 'left top', at: 'left bottom' },
-        blur: function () {
+        blur: function() {
             $(this).menu('option', 'position', { my: 'left top', at: 'left bottom' });
         },
-        focus: function (e, ui) {
+        focus: function(e, ui) {
             if ($('#menubar').get(0) !== $(ui).get(0).item.parent().get(0)) {
                 $(this).menu('option', 'position', { my: 'left top', at: 'right top' });
             }
         },
     });
+});
+let $sideA = $(`#tb-input`)
+$sideA.on("keyup", function() {
+    let $val = $sideA.val();
+    let container = $(`#container`);
+    if (isNaN($val)) {
+        container.text(`That is not a number!`);
+    } else {
+        container.text(` ${$val * 4}`);
+
+    }
+
+});
+
+let info = new Vue({
+    el: `#info`,
+    data: {
+        header: `The perimeter of Square`,
+        information: `A Square is a regular rectangle but have four equal sides A with 90 degrees angles `,
+        formula: `P = 4A`,
+        explanation: `A is the length of one of the sides.`
+    }
 });
