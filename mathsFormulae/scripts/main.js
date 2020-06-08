144f8f5f5b36d060d67dc9bee4c9d9d9792e8b7c
kaloyanbobev <kaloyanbobev@gmail.com>
2020-06-08 19:04:21 +0100

    separete info in other file

--- mathsFormulae/scripts/main.js
+++ mathsFormulae/scripts/main.js
@@ -77,6 +77,7 @@ function calculateRectangle(sideA, sideB, container) {
         }
     });
 };
+
 let btnPolygon = $(`#btn-polygon`);
 
 function calculatePolygon(sideA, numberSides, container) {
@@ -98,52 +99,3 @@ calculateWithOneVariable($(`#tb-input-radius`), $(`#container-circle`), 2 * Math
 calculateTriangle($(`#tb-input-triangle-a`), $(`#tb-input-triangle-b`), $(`#tb-input-triangle-c`), $(`#container-triangle`));
 calculateRectangle($(`#tb-input-rectangle-a`), $(`#tb-input-rectangle-b`), $(`#container-rectangle`));
 calculatePolygon($(`#tb-input-polygon-a`), $(`#tb-input-polygon-numbers`), $(`#container-polygon`));
-let infoSquare = new Vue({
-    el: `#infoSquare`,
-    data: {
-        header: `The Perimeter of Square`,
-        information: `A Square is a regular rectangle but have four equal sides A with 90 degrees angles `,
-        formula: `P = 4 * A`,
-        explanation: `A is the length of one of the sides.`
-    }
-});
-
-let infoCircle = new Vue({
-    el: `#infoCircle`,
-    data: {
-        header: `The perimeter of Cirlce`,
-        information: `Perimeter is a length around the circle `,
-        formula: `P = 2 *π * R`,
-        explanation: `π is constant  equal on 3,1415926535, R is a radius of a circle .`
-    }
-});
-
-let infoTriangle = new Vue({
-    el: `#infoTriangle`,
-    data: {
-        header: `The Perimeter of Triangle`,
-        information: `Perimeter is the sum of three sides of triagle`,
-        formula: `P = A + B + C`,
-        explanation: `A, B and C are the length of each of the sides.`
-    }
-});
-
-let infoRectangle = new Vue({
-    el: `#infoRectangle`,
-    data: {
-        header: `The Perimeter of Rectangle`,
-        information: `Perimeter is the sum of two times length and two times width `,
-        formula: `P = 2 * (A + B) `,
-        explanation: `A is a length and B is a width.`
-    }
-});
-
-let infoPolygon = new Vue({
-    el: `#infoPolygon`,
-    data: {
-        header: `The Perimeter of regular Polygon`,
-        information: `Perimeter is the sum all equal sides of polygon`,
-        formula: `P = X * A `,
-        explanation: `A is a length of one of the sides and X is number of the sides.`
-    }
-});
