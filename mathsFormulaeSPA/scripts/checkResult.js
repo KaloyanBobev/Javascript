fe843ae685b07e798ce8e43fbb6729623f71c271
kaloyanbobev <kaloyanbobev@gmail.com>
2020-06-23 11:52:06 +0100

    Update checkResult.js

--- mathsFormulaeSPA/scripts/checkResult.js
+++ mathsFormulaeSPA/scripts/checkResult.js
@@ -1,17 +1,16 @@
-  function checkResult() {
+  (function checkResult() {
       let ckeckBtn = $(`#check-btn`);
       let array = [34.54, 26, 31.5, 25.2, 46.8];
       ckeckBtn.on(`click`, function() {
-          for (let i = 0; i < 5; i += 1) {
+          for (let i = 0; i < array.length; i += 1) {
               let element = $(`#answer-${i}`);
-              if (element.val() * 1 === array[i]) {
-                  element.css("border", "2px solid lightgreen");
-                  element.next().text(`Correct`).css("color", "lightgreen").addClass("glyphicon glyphicon-ok");
+              if (+element.val() === array[i]) {
+                  element.css("border", "3px solid green");
+                  element.next().text(`Correct`).css("color", "green");
               } else {
-                  element.css("border", "2px solid red");
-                  element.next().text(`incorrect - a correct answer is ${array[i]}`).css("color", "red");;
+                  element.css("border", "3px solid red");
+                  element.next().text(`incorrect - a correct answer is ${array[i]}`).css("color", "red");
               }
           }
       })
-  };
-  checkResult();
+  })();
