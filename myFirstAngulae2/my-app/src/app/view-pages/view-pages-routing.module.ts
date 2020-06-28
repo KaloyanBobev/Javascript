1a982b4853f5af92b599f414755ab9f0c37c6582
kaloyanbobev <kaloyanbobev@gmail.com>
2020-06-28 17:25:59 +0100

    Update view-pages-routing.module.ts

--- myFirstAngulae2/my-app/src/app/view-pages/view-pages-routing.module.ts
+++ myFirstAngulae2/my-app/src/app/view-pages/view-pages-routing.module.ts
@@ -2,11 +2,12 @@ import { HomeComponent } from './home.component';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TheoryComponent } from './theory.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'theory', component: NativeComponent },
+    { path: 'theory', component: TheoryComponent },
 
 ];
 
@@ -14,4 +15,4 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class ViewStrategiesRoutingModule { }
+export class ViewPagesRoutingModule { }
