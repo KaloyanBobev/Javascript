35ff83b36fc18076c4ca14dc0dd702f26d8dff49
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-01 17:25:10 +0100

    new routing and service modules

--- todo-app/src/app/todos/todo.module.ts
+++ todo-app/src/app/todos/todo.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
-import { TodoComponent } from './todo.component';
+import { TodoComponent } from './todo/todo.component';
 
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [TodoComponent],
+  declarations: [TodoComponent,
+    TodoListComponent],
   imports: [
     CommonModule,
-    TodoRoutingModule
+    TodoRoutingModule,
   ]
 })
 export class TodoModule { }
