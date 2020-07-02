d77d551fa9a2115627acd21f4142319434fefba3
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-02 10:54:29 +0100

    moving data to service file

--- todo-app/src/app/todos/todo.service.ts
+++ todo-app/src/app/todos/todo.service.ts
@@ -5,5 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class TodoService {
 
+  todos = [
+    { id: 1, name: 'Milk', dueDate: '08/08/2020' },
+    { id: 2, name: 'Beer', dueDate: '09/09/2020' },
+    { id: 3, name: 'Chips', dueDate: '09/09/2020' },
+  ];
   constructor() { }
+
+  getAll() {
+    return this.todos;
+  }
 }
