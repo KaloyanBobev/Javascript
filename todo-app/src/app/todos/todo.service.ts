09cb0e9eac586f91fdd1e4df8a11cb2feea13953
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-04 19:43:06 +0100

    Update todo.service.ts

--- todo-app/src/app/todos/todo.service.ts
+++ todo-app/src/app/todos/todo.service.ts
@@ -6,9 +6,9 @@ import { Injectable } from '@angular/core';
 export class TodoService {
 
   todos = [
-    { id: 1, name: 'Milk', dueDate: '08/08/2020' },
-    { id: 2, name: 'Beer', dueDate: '09/09/2020' },
-    { id: 3, name: 'Chips', dueDate: '09/09/2020' },
+    { id: 1, name: 'Milk', done: false, dueDate: '08/08/2020' },
+    { id: 2, name: 'Beer', done: false, dueDate: '09/09/2020' },
+    { id: 3, name: 'Chips', done: false, dueDate: '09/09/2020' },
   ];
   constructor() { }
 
