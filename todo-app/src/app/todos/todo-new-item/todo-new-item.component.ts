819b109be2d375e2d708e5e34e8e27ae1d4f17ee
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-05 17:54:59 +0100

    Create todo-new-item.component.ts

---
+++ todo-app/src/app/todos/todo-new-item/todo-new-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-new-item',
+  templateUrl: './todo-new-item.component.html',
+  styleUrls: ['./todo-new-item.component.css']
+})
+export class TodoNewItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
