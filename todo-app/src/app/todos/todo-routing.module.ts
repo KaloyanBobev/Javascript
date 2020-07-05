0db1ecdf3420606842be38833edb39e9f6d2ac6d
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-05 21:10:23 +0100

    Update todo-routing.module.ts

--- todo-app/src/app/todos/todo-routing.module.ts
+++ todo-app/src/app/todos/todo-routing.module.ts
@@ -1,11 +1,13 @@
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoNewItemComponent } from './todo-new-item/todo-new-item.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'all', pathMatch: 'full' },
-  { path: 'all', component: TodoListComponent }
+  { path: 'all', component: TodoListComponent },
+  { path: 'new', component: TodoNewItemComponent }
 ];
 
 @NgModule({
