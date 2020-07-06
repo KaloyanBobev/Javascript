8ed56360e74f0602e5db88fdbf1a0b78a900f2e4
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-06 11:58:07 +0100

    adding new method for add todo

--- todo-app/src/app/todos/todo.service.ts
+++ todo-app/src/app/todos/todo.service.ts
@@ -4,15 +4,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TodoService {
-
+  counter = 4;
   todos = [
-    { id: 1, name: 'Milk', done: false, dueDate: '08/08/2020' },
-    { id: 2, name: 'Beer', done: false, dueDate: '09/09/2020' },
-    { id: 3, name: 'Chips', done: false, dueDate: '09/09/2020' },
+    { id: 1, name: 'Milk', dueDate: '08/08/2020' },
+    { id: 2, name: 'Beer', dueDate: '09/09/2020' },
+    { id: 3, name: 'Chips', dueDate: '09/09/2020' },
   ];
-  constructor() { }
+  constructor() {
+
+  }
 
   getAll() {
     return this.todos;
+  }
+  addTodo(newName: string, newDueDate: string) {
+
+    const newId = 0;
+    const newTodo = {
+      id: newId + this.counter,
+      name: newName,
+      dueDate: newDueDate
+    };
+
+    this.counter += 1;
+    this.todos.push(newTodo);
   }
 }
