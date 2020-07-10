777a51006e4e0cc66aeda1895140ea50e0faee18
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-10 10:17:55 +0100

    adiing routes

--- demo-app/src/app/app-routing.module.ts
+++ demo-app/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { ListComponent } from './list/list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'list', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
