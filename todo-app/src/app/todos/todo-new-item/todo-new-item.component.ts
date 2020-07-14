8bf0bbd8a8fdb681f4c7ec37e5830ac9aa8c50e6
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-14 11:20:44 +0100

    add det and set methods

--- todo-app/src/app/todos/todo-new-item/todo-new-item.component.ts
+++ todo-app/src/app/todos/todo-new-item/todo-new-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, SimpleChange } from '@angular/core';
 import { TodoService } from './../todo.service';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-todo-new-item',
@@ -8,15 +9,43 @@ import { TodoService } from './../todo.service';
 })
 export class TodoNewItemComponent implements OnInit {
   title = 'Add new todo task';
+  @Input()
+  _newName: string;
+  @Input()
+  _newDate: string;
+
   todos;
 
   constructor(private TodoService: TodoService) { }
 
+  get newName(): string {
+    return this._newName;
+  }
+  set newName(value: string) {
+    if (value.length < 2) {
+      throw new Error('The task name is short');
+    }
+    this._newName = value;
+  }
+
+  get newDate(): string {
+    return this._newDate;
+  }
+
+  set newDate(value: string) {
+    if (!value) {
+      throw new Error('Enter valid date');
+    }
+    this._newDate = value;
+  }
+
   ngOnInit(): void {
   }
 
-  addNewItem(newName: string, newDueDate: string) {
-    this.todos = this.TodoService.addTodo(newName, newDueDate);
+  addNewItem() {
+    const newName = this._newName;
+    const newDate = this._newDate;
+    this.todos = this.TodoService.addTodo(newName, newDate);
   }
 
 }
