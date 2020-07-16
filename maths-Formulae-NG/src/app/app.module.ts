a316a8bb0d25532b92ccf339188a97b24eb2723d
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-16 13:35:24 +0100

    adding  pages components

--- maths-Formulae-NG/src/app/app.module.ts
+++ maths-Formulae-NG/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TheoryComponent } from './theory/theory.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PerimeterComponent } from './perimeter/perimeter.component';
+import { AreaComponent } from './area/area.component';
+import { MathsQuizComponent } from './maths-quiz/maths-quiz.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheoryComponent,
+    PageNotFoundComponent,
+    PerimeterComponent,
+    AreaComponent,
+    MathsQuizComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
