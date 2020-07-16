410a56eca533d6cd300cda9292d3211e45f46b00
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-16 13:32:19 +0100

    adding new routes

--- maths-Formulae-NG/src/app/app-routing.module.ts
+++ maths-Formulae-NG/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { MathsQuizComponent } from './maths-quiz/maths-quiz.component';
+import { AreaComponent } from './area/area.component';
+import { PerimeterComponent } from './perimeter/perimeter.component';
+import { TheoryComponent } from './theory/theory.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'theory', pathMatch: 'full' },
+  { path: 'theory', component: TheoryComponent },
+  { path: 'perimeter', component: PerimeterComponent },
+  { path: 'area', component: AreaComponent },
+  { path: 'quiz', component: MathsQuizComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
