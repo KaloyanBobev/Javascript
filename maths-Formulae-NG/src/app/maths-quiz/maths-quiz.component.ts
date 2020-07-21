b4e86da638852a1c4e2fdaab74b1aa4cc9e2b723
kaloyanbobev <kaloyanbobev@gmail.com>
2020-07-21 21:36:07 +0100

    adding checkREsult func

--- maths-Formulae-NG/src/app/maths-quiz/maths-quiz.component.ts
+++ maths-Formulae-NG/src/app/maths-quiz/maths-quiz.component.ts
@@ -9,11 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class MathsQuizComponent implements OnInit {
 
   questions;
-
+  color;
+  array = [34.54, 26, 31.5, 25.2, 46.8];
   constructor(private MathsQuizService: MathsQuizService) { }
 
   ngOnInit() {
     this.questions = this.MathsQuizService.getAll();
   }
 
+  ngOnChanges() {
+    this.checkResult;
+  }
+
+
+  checkResult(result, index) {
+
+
+    if (+result === this.array[index]) {
+      return this.color = 'green';
+    } else {
+      return this.color = 'red';
+    }
+  }
 }
