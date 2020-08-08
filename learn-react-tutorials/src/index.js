6f0f95a19ce106a022572b74e985379cb50ba242
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-08 19:30:48 +0100

    adding new project

---
+++ learn-react-tutorials/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+ReactDom.render(<div><h1>Hello world!</h1><p>Paragraht</p></div>,
+    document.getElementById('root'));
