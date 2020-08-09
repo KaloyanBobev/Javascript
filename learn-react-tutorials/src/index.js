0140dea0efc06393d7ba21dd014853486a68a2cd
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-09 18:37:15 +0100

    start new practice

--- learn-react-tutorials/src/index.js
+++ learn-react-tutorials/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-import App from './components/App';
 
 ReactDom.render(
-    < App />,
+    <App />,
     document.getElementById('root')
+
 );
