44ba468cc0d9974f53f6e6a156b89834e337bee2
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-09 18:26:19 +0100

    new Heater, Main and Fotter component

---
+++ learn-react-tutorials/src/components/Maincontent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main>
+            <p>this is my content</p>
+        </main>
+    );
+};
+
+export default MainContent;
