c7eece25d9a5830525e77d9abe84214e73131607
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-09 09:59:16 +0100

    new component MyInfo

---
+++ learn-react-tutorials/src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <article>
+            <h1>Kaloyan</h1>
+            <p>Some biography</p>
+            <ol>
+                <li>Rome</li>
+                <li>Liverpool</li>
+                <li>Jamaica</li>
+            </ol>
+        </article>
+    )
+}
+export default MyInfo;
