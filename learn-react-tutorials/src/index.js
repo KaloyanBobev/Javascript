f8a308d265c50e0e9957742fe0b725bc35b0dab5
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-09 18:32:36 +0100

    update

--- learn-react-tutorials/src/index.js
+++ learn-react-tutorials/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-import MyInfo from './components/MyInfo';
-
+import App from './components/App';
 
 ReactDom.render(
-    <MyInfo />,
+    < App />,
     document.getElementById('root')
 );
