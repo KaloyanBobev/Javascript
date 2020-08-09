11dfc011bb3bb98a312d21eecf6ba5ac1ced7d53
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-09 09:59:35 +0100

    remove component MyInfo

--- learn-react-tutorials/src/index.js
+++ learn-react-tutorials/src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
+import MyInfo from './components/MyInfo';
 
-function MyInfo() {
-    return (
-        <article>
-            <h1>Kaloyan</h1>
-            <p>Some biography</p>
-            <ol>
-                <li>Rome</li>
-                <li>Liverpool</li>
-                <li>Jamaica</li>
-            </ol>
-        </article>
-    )
-}
 
 ReactDom.render(
     <MyInfo />,
