75f75e5b7574b0914d60034bf440fba3536f353a
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-09 18:49:27 +0100

    new task

---
+++ learn-react-tutorials/src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Maincontent() {
+    return (
+        <article>
+            <h2>Title</h2>
+            <p>Content</p>
+        </article>
+    )
+
+};
+
+export default Maincontent;
---
+++ learn-react-tutorials/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <h2>LOGO</h2>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navbar;
