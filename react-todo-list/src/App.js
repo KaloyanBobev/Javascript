f75a08dbbb257f57252b97a589785b552e6a78c2
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-10 17:14:16 +0100

    move the html to todo component

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
-    let task = 'Task';
+
     return (
-        <ul>
-            <li>
-                <input type="checkbox"></input>
-                <span> {task} 1</span>
-                <p>Beer</p>
-            </li>
-            <li>
-                <input type="checkbox"></input>
-                <span> {task} 2</span>
-                <p>Meat</p>
-            </li>
-            <li>
-                <input type="checkbox"></input>
-                <span> {task} 3</span>
-                <p>Eggs</p>
-            </li>
-            <li>
-                <input type="checkbox"></input>
-                <span> {task} 4</span>
-                <p>Bread</p>
-            </li>
-        </ul>
+        <TodoList />
     )
 }
 
