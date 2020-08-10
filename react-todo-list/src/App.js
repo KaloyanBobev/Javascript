bad2a6819659c3933c0e934f6b610a272a38d27e
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-10 15:46:51 +0100

    add todo list

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 function App() {
-    return (<div>APP</div>)
+    let task = 'Task';
+    return (
+        <ul>
+            <li>
+                <input type="checkbox"></input>
+                <span> {task} 1</span>
+                <p>Beer</p>
+            </li>
+            <li>
+                <input type="checkbox"></input>
+                <span> {task} 2</span>
+                <p>Meat</p>
+            </li>
+            <li>
+                <input type="checkbox"></input>
+                <span> {task} 3</span>
+                <p>Eggs</p>
+            </li>
+            <li>
+                <input type="checkbox"></input>
+                <span> {task} 4</span>
+                <p>Bread</p>
+            </li>
+        </ul>
+    )
 }
 
 export default App;
