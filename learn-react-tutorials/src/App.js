f4c39a0a41dfe616c34a69ba080e9678602cec0e
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-11 17:12:54 +0100

    delete the old task

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 
-import Joke from './components/Joke';
-import jokesData from './JokesData';
 
 function App() {
-    const jokeComponents = jokesData.map((joke) => {
-        return (
-            <Joke key={joke.id} question={joke.question}
-                punctLine={joke.punctLine}
-            />
-        )
-    })
+
 
     return (
-        <div className="container">
-            {jokeComponents}
+        <div>
+            APP
         </div>
     )
 }
