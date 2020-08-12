92ee2bb45264885283baf6a71ef723efc0748413
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-12 10:30:16 +0100

    update a ligic

--- react-todo-list/src/components/TodoList.js
+++ react-todo-list/src/components/TodoList.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import TodoItem from './TodoItem';
-function TodoList() {
 
-    const date = new Date(2020, 8, 10, 18);
-    const hours = date.getHours();
-    let timeOfDay;
-
-    const styles = {
-        fontSize: '2.5em'
-    };
-
-    if (hours < 12) {
-        timeOfDay = 'morning';
-        styles.color = 'green';
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = 'afternoon';
-        styles.color = 'darkblue';
-    } else {
-        timeOfDay = 'night';
-        styles.color = 'darkred';
-    }
+function TodoList(props) {
 
     return (
         <div>
-            <h1 style={styles}>Good {timeOfDay}!</h1>
-            <ul>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-            </ul>
+            <li className="todolist">
+                <input type="checkbox" checked={props.product.complated}></input>
+                <label>Task: {props.product.text}</label>
+
+            </li>
         </div>
     )
 }
