14b58d2de78fb6c107f1b7556167a5518b0402dd
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-12 10:28:02 +0100

    improving passing the data

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import todoData from './todoData';
 
 function App() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
 
+    const styles = {
+        fontSize: '2.5em'
+    };
+
+    if (hours < 12) {
+        timeOfDay = 'morning';
+        styles.color = 'green';
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = 'afternoon';
+        styles.color = 'darkblue';
+    } else {
+        timeOfDay = 'night';
+        styles.color = 'darkred';
+    }
+    const todoComponents = todoData.map((item) => {
+        return (
+            <TodoList key={item.id}
+                product={item}
+            />
+        )
+    })
     return (
-        <TodoList />
+        <div>
+            <h1 style={styles}>Good {timeOfDay}!</h1>
+            {todoComponents}
+        </div>
     )
 }
 
