e980514c9c58b4ddec73da46bcbe225e0f95a514
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-13 18:46:42 +0100

    Ready for new task

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            answer: 'Yes'
-        }
-    }
     render() {
         return (
-            <div>
-                <h1>Is state important to know? {this.state.answer}</h1>
-            </div>
+            <div>APP</div>
         )
     }
 }
-
 export default App;
