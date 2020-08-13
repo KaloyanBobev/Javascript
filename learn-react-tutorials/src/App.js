4eb2ac7c0e09e8f7dff6ecbbcd772ecd5764e508
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-13 18:17:59 +0100

    finish this task

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 
 class App extends React.Component {
-
-    yourMethosHere() {
-        return (<h1>Title</h1>)
-    }
-
     render() {
-        const style = this.yourMethosHere();
         return (
-            <div>{style}</div>
+            <div>
+                <Header username='Gosho' />
+                <Greeting />
+            </div>
+        )
+    }
+};
+class Header extends Component {
+    render() {
+
+        return (
+
+            <header>
+                <p>Welcome, {this.props.username}!</p>
+            </header>
         )
     }
 }
+class Greeting extends Component {
 
+    whatTimeOfDay(hours, style) {
+        if (hours < 12) {
+            return "morning";
+
+        } else if (hours > 12 && hours < 17) {
+            return 'aftornnon';
+        } else {
+            return 'night';
+        }
+    }
+
+    render() {
+        const date = new Date();
+        const hours = date.getHours();
+        let timeOfDay = this.whatTimeOfDay(hours);
+
+        return (
+            <h2>
+                Good { timeOfDay}, to you sir or madam!
+            </h2>
+        )
+    }
+}
 export default App;
