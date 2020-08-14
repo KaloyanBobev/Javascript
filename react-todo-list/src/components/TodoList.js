66b0dd5e6ca4c02955937a7cabfc5607ad789bce
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-14 18:53:15 +0100

    add onClick event

--- react-todo-list/src/components/TodoList.js
+++ react-todo-list/src/components/TodoList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-function TodoList(props) {
+class TodoList extends React.Component {
 
-    return (
-        <div>
-            <li className="todolist">
-                <input type="checkbox" checked={props.product.complated}></input>
-                <label>Task: {props.product.text}</label>
+    render() {
+        return (
+            <div>
+                <li className="todolist">
+                    <input type="checkbox" onClick={() => {
+                        alert('changed')
+                    }} checked={this.props.product.complated}></input>
+                    <label>Task: {this.props.product.text}</label>
 
-            </li>
-        </div>
-    )
+                </li>
+            </div>
+        )
+    }
 }
 export default TodoList;
