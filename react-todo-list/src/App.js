128a6a3b209b1eab18a056a403d0b007e81c85c6
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-14 16:03:15 +0100

    change to class and add constructor, andstate

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -2,38 +2,50 @@ import React from 'react';
 import TodoList from './components/TodoList';
 import todoData from './todoData';
 
-function App() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
+class App extends React.Component {
 
-    const styles = {
-        fontSize: '2.5em'
-    };
-
-    if (hours < 12) {
-        timeOfDay = 'morning';
-        styles.color = 'green';
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = 'afternoon';
-        styles.color = 'darkblue';
-    } else {
-        timeOfDay = 'night';
-        styles.color = 'darkred';
+    constructor() {
+        super();
+        this.state = {
+            todos: todoData
+        }
     }
-    const todoComponents = todoData.map((item) => {
+
+    render() {
+        const date = new Date();
+        const hours = date.getHours();
+        let timeOfDay;
+
+        const styles = {
+            fontSize: '2.5em'
+        };
+
+        const todoComponents = this.state.todos.map((item) => {
+            return (
+                <TodoList key={item.id}
+                    product={item}
+                />
+            )
+        })
+
+        if (hours < 12) {
+            timeOfDay = 'morning';
+            styles.color = 'green';
+        } else if (hours >= 12 && hours < 17) {
+            timeOfDay = 'afternoon';
+            styles.color = 'darkblue';
+        } else {
+            timeOfDay = 'night';
+            styles.color = 'darkred';
+        }
+
         return (
-            <TodoList key={item.id}
-                product={item}
-            />
+            <div>
+                <h1 style={styles}>Good {timeOfDay}!</h1>
+                {todoComponents}
+            </div>
         )
-    })
-    return (
-        <div>
-            <h1 style={styles}>Good {timeOfDay}!</h1>
-            {todoComponents}
-        </div>
-    )
+    }
 }
 
 export default App;
