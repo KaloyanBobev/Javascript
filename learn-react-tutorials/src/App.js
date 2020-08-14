efb94c94813ca0cba4b73e91f44b211b88d42b77
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-14 18:25:38 +0100

    finished with event handler

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+function handleClick() {
+
+    alert("Clicked");
+
+}
+
+function hovered() {
+    alert('hovered')
+}
+
 class App extends React.Component {
+
+
     render() {
         return (
-            <div>APP</div>
+            <div>
+                <img onMouseOver={hovered} alt="bus" src="https://image.freepik.com/free-vector/vector-illustration-mountain-landscape_1441-71.jpg"></img>
+                <br></br>
+                <br></br>
+                <button onClick={handleClick}>Click me</button>
+            </div>
         )
     }
 }
