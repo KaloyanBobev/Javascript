2ae386677715449eeba75869978d67cd7d3bc741
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-15 19:30:58 +0100

    finish with lifecycle part 1

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -3,8 +3,28 @@ import React from 'react';
 class App extends React.Component {
     constructor() {
         super();
+        this.state = {};
     }
 
+    componentDidMount() {
+        //GET the data I need to currenctly display
+    }
+
+    /*UNSAFE*/componentWillReceiveProps(nextProps) {
+        if (nextProps.whatever !== this.props.whatever) {
+            //do somethimg important here
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        //return true if want it to update
+        //return falhe if not 
+    }
+
+    componentWillUnmount() {
+        //teardown or cleanup your code before your component disapaer
+        //(e.g. remove event listener)
+    }
 
     render() {
         return (
