c5633ab2ecba85d73330a35eccde514e90a28757
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-15 20:01:38 +0100

    end of lifecycle part 2

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -6,20 +6,39 @@ class App extends React.Component {
         this.state = {};
     }
 
+    static getDerivedStateFromProps(state, props) {
+        //return the new, updated state based upon the props
+    }
+
+    getSnapshotBeforeUpdate() {
+        //create a backup of thr current way things are
+    }
+
+    //deprecated
+    //     componentWillMount() {
+
+    // }
+
     componentDidMount() {
         //GET the data I need to currenctly display
     }
 
-    /*UNSAFE*/componentWillReceiveProps(nextProps) {
-        if (nextProps.whatever !== this.props.whatever) {
-            //do somethimg important here
-        }
-    }
+    //deprecated
+    // componentWillReceiveProps(nextProps) {
+    //     if (nextProps.whatever !== this.props.whatever) {
+    //         //do somethimg important here
+    //     }
+    // }
 
     shouldComponentUpdate(nextProps, nextState) {
         //return true if want it to update
         //return falhe if not 
     }
+
+    //deprecated
+    //     componentWillUpdate() {
+
+    // }
 
     componentWillUnmount() {
         //teardown or cleanup your code before your component disapaer
