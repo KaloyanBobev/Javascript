d7ae92f1de9427c78f8c8e3b2a4f3f11b1e1746a
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-16 18:37:30 +0100

    start of fetching data from an API

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import Conditional from './components/Conditional'
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            isLoggedIn: false,
-
         }
-        this.handleClick = this.handleClick.bind(this);
+
     }
 
-    handleClick() {
-        this.setState((prevState) => {
-            return {
-                isLoggedIn: !prevState.isLoggedIn
-            }
-        })
-    }
 
 
 
     render() {
         return (
             <div>
-                {this.state.isLoggedIn === true ? <h1>Logged in</h1> :
-                    <Conditional />}
-                <button onClick={this.handleClick}>{this.state.isLoggedIn ? 'Log Out'.toLocaleUpperCase() : 'Log In'.toLocaleUpperCase()}</button>
+                CODE
             </div>
         )
     }
