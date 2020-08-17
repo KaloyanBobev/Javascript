b59338f70c4d0834769c69e0241c743d1874188c
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-17 19:45:08 +0100

    end of forms part 1

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -3,11 +3,30 @@ import React from 'react';
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            firstName: "",
+            lastName: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(event) {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value,
+
+
+        })
+    }
+
     render() {
         return (
-            <div>CODE</div>
+            <form>
+                <input type="text" value={this.state.firstName} name="firstName" placeholder="First Name" onChange={this.handleChange} />
+                <input type="text" value={this.state.lastName} name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+
         )
     }
 }
