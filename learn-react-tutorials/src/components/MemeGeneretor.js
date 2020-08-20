e8a433f328e8466ea5e41d279bf9c8d4f6f7a035
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-20 11:14:21 +0100

    new Header component

---
+++ learn-react-tutorials/src/components/MemeGeneretor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MemeGenerator extends React.Component {
+
+    render() {
+        return (
+            <h1>Meme Generator</h1>
+        )
+    }
+}
+export default MemeGenerator;
