2a4ae55636977f01cf6145634898ecd4c56bada4
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-20 11:15:07 +0100

    adding Header and MemeGenerator components

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from './components/Header'
+import MemeGenerator from './components/MemeGeneretor'
 
 class App extends React.Component {
 
     render() {
         return (
             <div>
-                Code
+                <Header />
+                <MemeGenerator />
             </div>
         )
     }
