882829d0e4c255ed47db900fc966ba056d873821
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-22 17:58:57 +0100

    prepare for nexr stage

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Header from './components/Header'
-import MemeGenerator from './components/MemeGeneretor'
 
 class App extends React.Component {
 
     render() {
         return (
             <div>
-                <Header />
-                <MemeGenerator />
+                CODE
             </div>
         )
     }
