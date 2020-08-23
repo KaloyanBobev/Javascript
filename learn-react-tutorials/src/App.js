c08450ec98752f356e4f1f594d1098e8dface980
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-23 12:43:51 +0100

    use arrow function in handleChange

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
-
+    constructor() {
+        super();
+        this.state = {
+            firstName: ""
+        }
+    }
+    //Change to use arrow function
+    handleChange = (event) => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+    }
     render() {
         return (
             <div>
-                CODE
+                <form>
+                    <input
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                        placeholder="First Name"
+
+                    />
+                </form>
+                <h1>{this.state.firstName}</h1>
             </div>
         )
     }
