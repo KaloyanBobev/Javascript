9a966b244be55e6e85f89ca90ec55f9f5e8856dd
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-24 19:30:34 +0100

    add onClick

--- learn-react-tutorials/src/components/LandingPage.js
+++ learn-react-tutorials/src/components/LandingPage.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import auth from './auth';
 
 export default class LandingPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    handleClick = (event) => {
+        auth.login(() => {
+            this.props.history.push("/app")
+        })
+    }
 
     render() {
         return (
             <div>
-                Landing Page
+                <h1> Landing Page</h1>
+                <button onClick={this.handleClick}>Login</button>
             </div>
         )
     }
