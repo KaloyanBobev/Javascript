223c990d05e312983b68e3afe8b9171d13e0af0c
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-24 19:29:45 +0100

    update

--- learn-react-tutorials/src/App.js
+++ learn-react-tutorials/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
 import AppLayout from './components/AppLayout';
+
 export default class App extends React.Component {
 
 
@@ -16,7 +17,6 @@ export default class App extends React.Component {
                     </ul>
                     <Route exact path="/" component={LandingPage} />
                     <Route exact path="/app" component={AppLayout} />
-
                 </div>
             </BrowserRouter>
         )
